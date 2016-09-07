35d717804286bde9e26572e762e1db255e4ca374
Ryan Daigle <ryan.daigle@gmail.com>
2016-09-07 09:47:05 -0400

    Add exgen file

---
+++ exgen.exs
@@ -0,0 +1,14 @@
+defmodule Plug.Default.Exgen do
+  alias Exgen.Project
+
+  def context(project) do
+    [
+      app_name: Project.target_base(project),
+      module: Project.module_name(project)
+    ]
+  end
+
+  def target_file(target_path, context) do
+    target_path |> String.replace("app_name", context[:app_name])
+  end
+end
